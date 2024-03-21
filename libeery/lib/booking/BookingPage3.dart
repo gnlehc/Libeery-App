db896accfffbb75537e328fbf1f049928c0e44b0
Christopher Verrell <c.verrell07@gmail.com>
2024-03-21 16:30:25 +0700

    feat: booking page tab 3 finished

--- libeery/lib/booking/BookingPage3.dart
+++ libeery/lib/booking/BookingPage3.dart
@@ -2,24 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main(){
-  BookingPage3();
+  runApp(BookingPage3());
 }
 
-class BookingPage3 extends StatelessWidget {
+class Loker{
+  bool dipilih;
+  Color color;
+
+  Loker
+  (
+    {
+      required this.dipilih, required this.color
+    }
+  );
+}
+
+class BookingPage3 extends StatefulWidget {
+  @override
+  BookingPage3State createState() => BookingPage3State();
+}
+
+class BookingPage3State extends State<BookingPage3> {
+  List<List<Loker>> lokerUser = List.generate(
+    5,
+    (index) => List.generate(
+      5 * 12,
+      (index) => Loker(dipilih: false, color: Color(0xff5EC762)),
+    ),
+  );
+
+  bool lokerDipilih = false;
+  bool levelDipilih = false;
+  int selectedColumn = -1;
+  int selectedRow = -1;
+  int selectedLevel = -1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
       body: Column(
         children: [
-          SizedBox(height: 50),
           Container(
-            height: 20,
-            color: Colors.indigo,
-            padding: EdgeInsets.symmetric(horizontal: 25),
-          ),
-
-          Container(
-            height: 130,
+            height: 100,
             padding: EdgeInsets.symmetric(horizontal: 25),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -34,7 +61,8 @@ class BookingPage3 extends StatelessWidget {
                 Text(
                   "Pilih loker yang ingin kamu tempati selama mengunjungi LKC Binus. Warna hijau berarti loker dapat kamu pilih sedangkan warna merah sebaliknya.", 
                   style: TextStyle(
-                    fontSize: 15,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300,
                   ),
                 ),
               ],
@@ -62,108 +90,102 @@ class BookingPage3 extends StatelessWidget {
               ],
             ),
           ),
-
-          Container(
-            height: 450,
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
+          
+          // untuk pemilihan level 1-5
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
               child: Row(
                 children: [
                   Container(
-                    width: 180,
-                    child: GridView.count(
-                      padding: EdgeInsets.only(top: 10),
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 5,
-                      crossAxisCount: 5,
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      children: List.generate(
-                        12 * 5,
-                        (index) => Container(
-                          width: 20,
-                          height: 20,
-                          decoration: BoxDecoration(
-                            color: Color(0xff5EC762),
-                            borderRadius: BorderRadius.circular(3),
+                    width: 50,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Column(
+                        children: List.generate(
+                          5, 
+                          (index) => GestureDetector(
+                            onTap: (){
+                              setState((){
+                                selectedLevel = index;
+                                levelDipilih = false;
+                              });
+                            },
+                            child: Container(
+                              height: 120,
+                              margin: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: selectedLevel == index ? Color(0xffF18700) : Color.fromARGB(255, 224, 224, 224),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "${index+1}",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: selectedLevel == index ? Color(0xffF1F1F1) : Color(0xff333333),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    )
                   ),
+                  
+                  // untuk memilih 60 kotak dari masing-masing level
                   Expanded(
                     child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 180, // Fixed height for the SingleChildScrollView
-                            padding: EdgeInsets.only(left: 20),
-                            child: GridView.count(
-                              mainAxisSpacing: 10,
-                              crossAxisSpacing: 5,
-                              crossAxisCount: 5,
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              children: List.generate(
-                                15 * 5,
-                                (index) => Container(
-                                  width: 20,
-                                  height: 20,
-                                  decoration: BoxDecoration(
-                                    color: Color(0xff5EC762),
-                                    borderRadius: BorderRadius.circular(3),
-                                  ),
+                      scrollDirection: Axis.vertical,
+                      child: GridView.count(
+                        padding: EdgeInsets.only(top: 10, left: 15),
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        crossAxisCount: 5,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        children: List.generate(
+                          60,
+                          (index) {
+                            int row = index ~/ 12;
+                            int column = index % 12;
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  for (int i = 0; i < lokerUser.length; i++) {
+                                    for (int j = 0; j < lokerUser[i].length; j++) {
+                                      lokerUser[i][j].color = Color(0xff5EC762);
+                                    }
+                                  }
+
+                                  if (lokerUser[row][column].color == Color(0xff5EC762)) {
+                                    lokerUser[row][column].color = Color(0xff0097DA);
+                                  } else {
+                                    lokerUser[row][column].color = Color(0xff5EC762);
+                                  }
+
+                                  selectedColumn = column+1;
+                                  selectedRow = row;
+                                  if(selectedColumn >= 10){
+                                    selectedColumn -= 10;
+                                    selectedRow += 1;
+                                  }
+                                });
+                              },
+                              
+                              child: Container(
+                                width: 20,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  color: lokerUser[row][column].color,
+                                  borderRadius: BorderRadius.circular(3),
                                 ),
                               ),
-                            ),
-                          ),
-                          Container(
-                            height: 200, // Fixed height for the SingleChildScrollView
-                            padding: EdgeInsets.only(top: 40, left: 60),
-                            child: GridView.count(
-                              mainAxisSpacing: 10,
-                              crossAxisSpacing: 5,
-                              crossAxisCount: 5,
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              children: List.generate(
-                                15 * 5,
-                                (index) => Container(
-                                  width: 20,
-                                  height: 20,
-                                  decoration: BoxDecoration(
-                                    color: Color(0xff5EC762),
-                                    borderRadius: BorderRadius.circular(3),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            height: 220, // Fixed height for the SingleChildScrollView
-                            padding: EdgeInsets.only(top: 40, left: 20),
-                            child: GridView.count(
-                              mainAxisSpacing: 10,
-                              crossAxisSpacing: 5,
-                              crossAxisCount: 5,
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              children: List.generate(
-                                15 * 5,
-                                (index) => Container(
-                                  width: 20,
-                                  height: 20,
-                                  decoration: BoxDecoration(
-                                    color: Color(0xff5EC762),
-                                    borderRadius: BorderRadius.circular(3),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+                            );
+                          },
+                        ),
                       ),
                     ),
                   ),
@@ -175,18 +197,22 @@ class BookingPage3 extends StatelessWidget {
           Container(
             height: 30,
             padding: EdgeInsets.symmetric(horizontal: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[
-                Text("Loker yang dipilih"),
-                Text(
-                  "213", 
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700
-                  ),
-                )
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:[
+                    Text("Loker yang dipilih"),
+                    Text(
+                      "${(selectedLevel+1).toString()} - ${selectedRow.toString() + selectedColumn.toString()}", 
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700
+                      ),
+                    ),
+                  ]
+                ),
               ]
-            )
+            ),
           ),
 
           Container(
