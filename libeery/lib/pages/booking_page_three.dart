69d7865fce649a9ee2a0b59bcb92cbe47aedbc2e
Christopher Verrell <c.verrell07@gmail.com>
2024-06-04 07:49:43 +0700

    fix: loading indicator in booking page 3

--- libeery/lib/pages/booking_page_three.dart
+++ libeery/lib/pages/booking_page_three.dart
@@ -22,22 +22,28 @@ class BookingPage3 extends StatefulWidget {
 class BookingPage3State extends State<BookingPage3> {
   GetAllMsLokerData listLoker = GetAllMsLokerData();
   List<bool> progressStatus = [false, false, true, false];
-
-  void getLoker() {
-    LokerService lokerService = LokerService();
-    try {
-      setState(() async {
-        listLoker = await lokerService.getLoker();
-      });
-    } catch (e) {
-      e.toString();
-    }
-  }
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
     getLoker();
+  }
+
+  void getLoker() async {
+    LokerService lokerService = LokerService();
+    try {
+      final lokerData = await lokerService.getLoker();
+      setState(() {
+        listLoker = lokerData;
+        isLoading = false; 
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      e.toString();
+    }
   }
 
   List<List<Loker>> lokerUser = List.generate(
@@ -145,185 +151,193 @@ class BookingPage3State extends State<BookingPage3> {
             ),
           ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 120,
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        body: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Pilih Lokermu!",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      "Pilih loker yang ingin kamu tempati selama mengunjungi LKC Binus. Angka 1 - 5 menunjukkan letak baris loker, dimana 1 berarti baris teratas dan 5 adalah baris terbawah.",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 30,
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    AvailabilityInfo(
-                      information: "Tersedia",
-                      colorInfo: Color(0xff5EC762),
-                    ),
-                    AvailabilityInfo(
-                      information: "Tidak Tersedia",
-                      colorInfo: Color(0xffC75E5E),
-                    ),
-                    AvailabilityInfo(
-                      information: "Pilihanmu",
-                      colorInfo: Color(0xff0097DA),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 300,
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Row(
-                      children: [
-                        const SizedBox(
-                            height: 320,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                RowInfo(text: "1"),
-                                RowInfo(text: "2"),
-                                RowInfo(text: "3"),
-                                RowInfo(text: "4"),
-                                RowInfo(text: "5")
-                              ],
-                            )),
-                        Expanded(
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 60,
-                            itemBuilder: (context, columnIndex) {
-                              return Column(
-                                children: List.generate(5, (rowIndex) {
-                                  MsLoker? loker;
-                                  if (listLoker.data != null) {
-                                    loker = listLoker.data!.firstWhere(
-                                      (element) =>
-                                          element.rowNumber == rowIndex + 1 &&
-                                          element.columnNumber ==
-                                              columnIndex + 1,
-                                      orElse: () => MsLoker(
-                                          lockerID: 0,
-                                          rowNumber: 0,
-                                          columnNumber: 0,
-                                          availability: 'Active',
-                                          stsrc: ''),
-                                    );
-                                  }
-
-                                  Color color;
-                                  if (loker?.availability == 'Booked') {
-                                    color = const Color(0xffC75E5E);
-                                  } else {
-                                    if (lokerUser[rowIndex][columnIndex]
-                                        .dipilih) {
-                                      color = const Color(0xff0097DA);
-                                    } else {
-                                      color = const Color(0xff5EC762);
-                                    }
-                                  }
-
-                                  return GestureDetector(
-                                      onTap: () {
-                                        updateSelectedLocker(
-                                            rowIndex, columnIndex);
-                                      },
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 16.5, left: 10),
-                                        child: Container(
-                                          width: 40,
-                                          height: 40,
-                                          color: color,
-                                        ),
-                                      ));
-                                }),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    )),
-              ),
-              Container(
-                height: 60,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-                child: Column(children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text("Loker yang dipilih"),
-                        if (selectedRow != -1 && selectedColumn != -1)
+                    Container(
+                      height: 120,
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
                           Text(
-                            listLoker.data!
-                                .firstWhere((loker) =>
-                                    loker.rowNumber == selectedRow + 1 &&
-                                    loker.columnNumber == selectedColumn + 1)
-                                .lockerID
-                                .toString(),
-                            style: const TextStyle(fontWeight: FontWeight.w700),
-                          ),
-                      ]),
-                  const Divider(
-                    thickness: 1.0,
-                    color: Color(0xff333333),
-                  )
-                ]),
-              ),
-              Container(
-                  height: 100,
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xffF18700),
-                              fixedSize: const Size(134, 20),
+                            "Pilih Lokermu!",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w600,
                             ),
-                            child: const Text("Selanjutnya",
-                                style: TextStyle(color: Color(0xffF1F1F1)))),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.pop(context, const BookForNow());
-                            },
-                            child: const Text("Sebelumnya...",
-                                style: TextStyle(
-                                  fontSize: 12.0,
-                                  fontWeight: FontWeight.w400,
-                                  decoration: TextDecoration.underline,
-                                  color: Color.fromRGBO(141, 141, 141, 1),
-                                  decorationThickness: 0.2,
-                                )))
-                      ])),
-            ],
-          ),
-        ));
+                          ),
+                          Text(
+                            "Pilih loker yang ingin kamu tempati selama mengunjungi LKC Binus. Angka 1 - 5 menunjukkan letak baris loker, dimana 1 berarti baris teratas dan 5 adalah baris terbawah.",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 30,
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          AvailabilityInfo(
+                            information: "Tersedia",
+                            colorInfo: Color(0xff5EC762),
+                          ),
+                          AvailabilityInfo(
+                            information: "Tidak Tersedia",
+                            colorInfo: Color(0xffC75E5E),
+                          ),
+                          AvailabilityInfo(
+                            information: "Pilihanmu",
+                            colorInfo: Color(0xff0097DA),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 300,
+                      child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: Row(
+                            children: [
+                              const SizedBox(
+                                  height: 320,
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      RowInfo(text: "1"),
+                                      RowInfo(text: "2"),
+                                      RowInfo(text: "3"),
+                                      RowInfo(text: "4"),
+                                      RowInfo(text: "5")
+                                    ],
+                                  )),
+                              Expanded(
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: 60,
+                                  itemBuilder: (context, columnIndex) {
+                                    return Column(
+                                      children: List.generate(5, (rowIndex) {
+                                        MsLoker? loker;
+                                        if (listLoker.data != null) {
+                                          loker = listLoker.data!.firstWhere(
+                                            (element) =>
+                                                element.rowNumber ==
+                                                    rowIndex + 1 &&
+                                                element.columnNumber ==
+                                                    columnIndex + 1,
+                                            orElse: () => MsLoker(
+                                                lockerID: 0,
+                                                rowNumber: 0,
+                                                columnNumber: 0,
+                                                availability: 'Active',
+                                                stsrc: ''),
+                                          );
+                                        }
+
+                                        Color color;
+                                        if (loker?.availability == 'Booked') {
+                                          color = const Color(0xffC75E5E);
+                                        } else {
+                                          if (lokerUser[rowIndex][columnIndex]
+                                              .dipilih) {
+                                            color = const Color(0xff0097DA);
+                                          } else {
+                                            color = const Color(0xff5EC762);
+                                          }
+                                        }
+
+                                        return GestureDetector(
+                                            onTap: () {
+                                              updateSelectedLocker(
+                                                  rowIndex, columnIndex);
+                                            },
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  top: 16.5, left: 10),
+                                              child: Container(
+                                                width: 40,
+                                                height: 40,
+                                                color: color,
+                                              ),
+                                            ));
+                                      }),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          )),
+                    ),
+                    Container(
+                      height: 60,
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                      child: Column(children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text("Loker yang dipilih"),
+                              if (selectedRow != -1 && selectedColumn != -1)
+                                Text(
+                                  listLoker.data!
+                                      .firstWhere((loker) =>
+                                          loker.rowNumber == selectedRow + 1 &&
+                                          loker.columnNumber ==
+                                              selectedColumn + 1)
+                                      .lockerID
+                                      .toString(),
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w700),
+                                ),
+                            ]),
+                        const Divider(
+                          thickness: 1.0,
+                          color: Color(0xff333333),
+                        )
+                      ]),
+                    ),
+                    Container(
+                        height: 100,
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xffF18700),
+                                    fixedSize: const Size(134, 20),
+                                  ),
+                                  child: const Text("Selanjutnya",
+                                      style:
+                                          TextStyle(color: Color(0xffF1F1F1)))),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context, const BookForNow());
+                                  },
+                                  child: const Text("Sebelumnya...",
+                                      style: TextStyle(
+                                        fontSize: 12.0,
+                                        fontWeight: FontWeight.w400,
+                                        decoration: TextDecoration.underline,
+                                        color:
+                                            Color.fromRGBO(141, 141, 141, 1),
+                                        decorationThickness: 0.2,
+                                      )))
+                            ])),
+                  ],
+                ),
+              ));
   }
 }
 
